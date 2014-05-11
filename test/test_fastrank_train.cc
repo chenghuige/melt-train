ca444363a9a4387380829a98cafc946b146b3804
chenghuige <chenghuige@c4ccc3fd-b13d-49bf-a705-f618d8f4424a>
2014-05-11 12:48:39 +0000

    modify
    git-svn-id: https://svn.baidu.com/myspace/sep/chenghuige/melt-train@124425 c4ccc3fd-b13d-49bf-a705-f618d8f4424a

---
+++ test/test_fastrank_train.cc
@@ -0,0 +1,44 @@
+/** 
+ *  ==============================================================================
+ * 
+ *          \file   test_fastrank_train.cc
+ *
+ *        \author   chenghuige   
+ *
+ *          \date   2014-05-11 20:48:20.312025
+ *  
+ *  \Description:
+ *
+ *  ==============================================================================
+ */
+
+#define _DEBUG
+#define private public
+#define protected public
+#include "common_util.h"
+
+using namespace std;
+using namespace gezi;
+DEFINE_int32(level, 0, "min log level");
+DEFINE_string(i, "", "input");
+DEFINE_string(o, "", "output");
+DEFINE_string(type, "simple", "");
+
+TEST(fastrank_train, func)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  int s = google::ParseCommandLineFlags(&argc, &argv, false);
+  if (FLAGS_log_dir.empty())
+    FLAGS_logtostderr = true;
+  FLAGS_minloglevel = FLAGS_level;
+  //boost::progress_timer timer;
+  
+  return RUN_ALL_TESTS();
+}
