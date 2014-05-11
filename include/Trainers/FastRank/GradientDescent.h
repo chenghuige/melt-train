1d94355a7b97b112ae22613e15b2709e6aebe124
chenghuige <chenghuige@c4ccc3fd-b13d-49bf-a705-f618d8f4424a>
2014-05-11 11:49:07 +0000

    modify
    git-svn-id: https://svn.baidu.com/myspace/sep/chenghuige/melt-train@124401 c4ccc3fd-b13d-49bf-a705-f618d8f4424a

--- include/Trainers/FastRank/GradientDescent.h
+++ include/Trainers/FastRank/GradientDescent.h
@@ -38,7 +38,7 @@ namespace gezi {
 			RegressionTree tree = TreeLearner->FitTargets(AdjustTargetsAndSetWeights());
 			if (AdjustTreeOutputsOverride == nullptr)
 			{
-				(IStepSearch*)(ObjectiveFunction.get())->AdjustTreeOutputs(tree, TreeLearner->Partitioning, TrainingScores);
+				((IStepSearch*)(ObjectiveFunction.get()))->AdjustTreeOutputs(tree, TreeLearner->Partitioning, TrainingScores);
 			}
 			else
 			{//@TODO
