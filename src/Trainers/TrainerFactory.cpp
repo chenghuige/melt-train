bf51ac3a74fa540712f79b49e5a07c2d873ff83d
chenghuige <chenghuige@c4ccc3fd-b13d-49bf-a705-f618d8f4424a>
2014-05-11 12:45:59 +0000

    modify
    git-svn-id: https://svn.baidu.com/myspace/sep/chenghuige/melt-train@124424 c4ccc3fd-b13d-49bf-a705-f618d8f4424a

--- src/Trainers/TrainerFactory.cpp
+++ src/Trainers/TrainerFactory.cpp
@@ -1,18 +1,38 @@
 #include "MLCore/TrainerFactory.h"
 #include "Trainers/SVM/LinearSVM.h"
+#include "Trainers/FastRank/BinaryClassificationFastRank.h"
 namespace gezi {
 
 TrainerPtr TrainerFactory::CreateTrainer(string name)
 {
 	boost::to_lower(name);
-	if (name == "linearsvm")
+	if (name == "linearsvm" || name == "svm)
 	{
+		LOG(INFO) << "Creating LinearSVM trainer";
 		return make_shared<LinearSVM>();
 	}
-/*	if (name == "fastrankbinaryclassification")
+	if (name == "fastrankbinaryclassification" ||
+		name = "fastrank" || name == "gbdt")
 	{
-
-	}*/
+		LOG(INFO) << "Creating FastRank/GBDT trainer";
+		return make_shared<BinaryClassificationFastRank>();
+	}
+	if (name == "kernalsvm" || name == "libsvm")
+	{
+		LOG(INFO) << "Creating KernalSVM trainer";
+	}
+	if (name == "randomforest")
+	{
+		LOG(INFO) << "Creating RandomForest trainer";
+	}
+	if (name == "logisticregression" || name == "logistic")
+	{
+		LOG(INFO) << "Creating LogisticRegression trainer";
+	}
+	if (name == "binaryneuralnetwork" || name == "neural" || name == "neuralnetwork)
+	{
+		LOG(INFO) << "Creating BinaryNeuralNetwork trainer";
+	}
 	LOG(WARNING) << name << " is not supported now, return nullptr";
 	return nullptr;
 }
