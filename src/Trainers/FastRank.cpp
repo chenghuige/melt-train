8dfbb0aca29ace1502279eb58ea9d3016b49bccc
chenghuige <chenghuige@c4ccc3fd-b13d-49bf-a705-f618d8f4424a>
2014-05-12 02:09:55 +0000

    modify
    git-svn-id: https://svn.baidu.com/myspace/sep/chenghuige/melt-train@124501 c4ccc3fd-b13d-49bf-a705-f618d8f4424a

---
+++ src/Trainers/FastRank.cpp
@@ -0,0 +1,28 @@
+/**
+ *  ==============================================================================
+ *
+ *          \file   FastRank.cpp
+ *
+ *        \author   chenghuige
+ *
+ *          \date   2014-05-12 10:09:15.775908
+ *
+ *  \Description:
+ *  ==============================================================================
+ */
+
+#ifndef FAST_RANK_CPP_
+#define FAST_RANK_CPP_
+
+
+class FastRank 
+{
+public:
+
+protected:
+private:
+
+};
+
+
+#endif  //----end of FAST_RANK_CPP_
