b51fe3e99032f561a52283e10c4641065ff82da2
chenghuige <chenghuige@c4ccc3fd-b13d-49bf-a705-f618d8f4424a>
2014-05-17 14:23:21 +0000

    modify
    git-svn-id: https://svn.baidu.com/myspace/sep/chenghuige/melt-train@125784 c4ccc3fd-b13d-49bf-a705-f618d8f4424a

---
+++ test/test_bit.cc
@@ -0,0 +1,44 @@
+/** 
+ *  ==============================================================================
+ * 
+ *          \file   test_bit.cc
+ *
+ *        \author   chenghuige   
+ *
+ *          \date   2014-05-17 22:23:11.830949
+ *  
+ *  \Description:
+ *
+ *  ==============================================================================
+ */
+
+#define _DEBUG
+#define private public
+#define protected public
+#include "common_util.h"
+
+using namespace std;
+using namespace gezi;
+DEFINE_int32(vl, 0, "vlog level");
+DEFINE_string(i, "", "input");
+DEFINE_string(o, "", "output");
+DEFINE_string(type, "simple", "");
+
+TEST(bit, func)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  google::InitGoogleLogging(argv[0]);
+  google::InstallFailureSignalHandler();
+  int s = google::ParseCommandLineFlags(&argc, &argv, false);
+  if (FLAGS_log_dir.empty())
+    FLAGS_logtostderr = true;
+  if (FLAGS_v == 0)
+		FLAGS_v = FALGS_vl;
+  
+  return RUN_ALL_TESTS();
+}
