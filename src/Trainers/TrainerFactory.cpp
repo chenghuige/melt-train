61f72e99b373c35ffa01a48a6bd5d9a7fc8a7858
chenghuige <chenghuige@c4ccc3fd-b13d-49bf-a705-f618d8f4424a>
2014-11-12 09:38:34 +0000

    modify
    git-svn-id: https://svn.baidu.com/myspace/sep/chenghuige/melt-train@185394 c4ccc3fd-b13d-49bf-a705-f618d8f4424a

--- src/Trainers/TrainerFactory.cpp
+++ src/Trainers/TrainerFactory.cpp
@@ -5,51 +5,62 @@
 #include "Trainers/RandomTrainer.h"
 namespace gezi {
 
-TrainerPtr TrainerFactory::CreateTrainer(string name)
-{
-	boost::to_lower(name);
-	if (name == "baselinelinearsvm" || name == "baselinesvm")
+	void TrainerFactory::PrintTrainersInfo()
 	{
-		VLOG(0) << "Creating BaselineLinearSVM trainer";
-		return make_shared<BaseLineLinearSVM>();
+		VLOG(0) << "BinaryClassification Trainers";
+		VLOG(0) << "[LinearSVM] -cl linearsvm or svm | ./melt -helpmatch LinearSVM.cpp";
+		VLOG(0) << "super fast, for classification with large number of features like text classification";
+		VLOG(0) << "[FastRank] -cl fastrank or fr or gbdt | ./melt -helpmatch FastRank.cpp";
+		VLOG(0) << "fast, best auc result for most classification problems with num features < 10^5";
+		VLOG(0) << "For per trainer parameters use, like LinearSVM just <./melt -helpmatch LinearSVM.cpp>, for other common parameters <./melt -helpmatch Melt>";
+		VLOG(0) << "The default trainer is LinearSVM, if use other trainers use -cl, eg. <./melt feature.txt -c train -cl gbdt> will train feature.txt using gbdt trainer";
 	}
-	if (name == "linearsvm" || name == "svm")
+
+	TrainerPtr TrainerFactory::CreateTrainer(string name)
 	{
-		VLOG(0) << "Creating LinearSVM trainer";
-		return make_shared<LinearSVM>();
+		boost::to_lower(name);
+		if (name == "baselinelinearsvm" || name == "baselinesvm")
+		{
+			VLOG(0) << "Creating BaselineLinearSVM trainer";
+			return make_shared<BaseLineLinearSVM>();
+		}
+		if (name == "linearsvm" || name == "svm")
+		{
+			VLOG(0) << "Creating LinearSVM trainer";
+			return make_shared<LinearSVM>();
+		}
+		if (name == "fastrankbinaryclassification" ||
+			name == "fastrank" || name == "gbdt" || name == "fr")
+		{
+			VLOG(0) << "Creating FastRank/GBDT trainer";
+			return make_shared<BinaryClassificationFastRank>();
+		}
+		if (name == "kernalsvm" || name == "libsvm")
+		{
+			VLOG(0) << "Creating KernalSVM trainer";
+		}
+		if (name == "randomforest")
+		{
+			VLOG(0) << "Creating RandomForest trainer";
+		}
+		if (name == "logisticregression" || name == "logistic")
+		{
+			VLOG(0) << "Creating LogisticRegression trainer";
+		}
+		if (name == "binaryneuralnetwork" || name == "neural" || name == "neuralnetwork")
+		{
+			VLOG(0) << "Creating BinaryNeuralNetwork trainer";
+		}
+		//just for test
+		if (name == "random")
+		{
+			VLOG(0) << "Creatring random trainer(do nothing will genearate a random predictor to predict 0,1 randomly)";
+			return make_shared<RandomTrainer>();
+		}
+
+		LOG(WARNING) << name << " is not supported now, return nullptr";
+		return nullptr;
 	}
-	if (name == "fastrankbinaryclassification" ||
-		name == "fastrank" || name == "gbdt" || name == "fr")
-	{
-		VLOG(0) << "Creating FastRank/GBDT trainer";
-		return make_shared<BinaryClassificationFastRank>();
-	}
-	if (name == "kernalsvm" || name == "libsvm")
-	{
-		VLOG(0) << "Creating KernalSVM trainer";
-	}
-	if (name == "randomforest")
-	{
-		VLOG(0) << "Creating RandomForest trainer";
-	}
-	if (name == "logisticregression" || name == "logistic")
-	{
-		VLOG(0) << "Creating LogisticRegression trainer";
-	}
-	if (name == "binaryneuralnetwork" || name == "neural" || name == "neuralnetwork")
-	{
-		VLOG(0) << "Creating BinaryNeuralNetwork trainer";
-	}
-	//just for test
-	if (name == "random")
-	{
-		VLOG(0) << "Creatring random trainer(do nothing will genearate a random predictor to predict 0,1 randomly)";
-		return make_shared<RandomTrainer>();
-	}
-	
-	LOG(WARNING) << name << " is not supported now, return nullptr";
-	return nullptr;
-}
 
 
 }  //----end of namespace gezi
